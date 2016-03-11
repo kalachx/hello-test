0692328a40ed381f3982ccc54f5436e809a0fdee
kalachx <kalach.x@poczta.fm>
2016-03-11 12:24:40 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void)
+int main(int a)
 {
-  printf("zmiana kodu o 11:38\n");
+  printf("arg %d\n", a)
+  printf("zmiana kodu o 12:24\n");
   long i;
   for(i=0;i<10;i++) printf("c%d\n", i);
   return 0;
