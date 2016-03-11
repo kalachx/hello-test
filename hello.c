7a859e08c66567865aa0e8fbb9d62203cc299da7
kalachx <kalach.x@poczta.fm>
2016-03-11 13:06:22 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main(int n, char** s)
 {
   printf("ilosc arg %d\n", n);
-  printf("zmiana kodu o 12:40\n");
+  printf("zmiana kodu o 13:06\n");
   long i, j;
   for(i=1;i<n;i++) 
   if (sscanf(s[i], "%d", &j)) printf("arg[%d] = '%s'\n", j);
