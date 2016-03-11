e5c739c348b36137a9dff65c4d3182bb4f3561ba
kalachx <kalach.x@poczta.fm>
2016-03-11 11:09:58 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 int main(void)
 {
   long i;
-  for(i=0;i<10;printf("a%d\n", i)i++,printf("b%d\n", i)) printf("c%d\n", i);
+  for(i=0;i<10;printf("a%d\n", i),i++,printf("b%d\n", i)) printf("c%d\n", i);
   return 1;
 }
