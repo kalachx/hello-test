83eeeb94ed47a61912e51da9eb9062d8eaa0ab53
kalachx <kalach.x@poczta.fm>
2016-03-11 13:05:42 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,8 +4,10 @@ int main(int n, char** s)
 {
   printf("ilosc arg %d\n", n);
   printf("zmiana kodu o 12:40\n");
-  long i;
-  for(i=1;i<n;i++) printf("arg[%d] = '%s'\n", i, s[i]);
-  if (sscanf(s[1], "%d", &i)) printf("i = %d\n", i);
+  long i, j;
+  for(i=1;i<n;i++) 
+  if (sscanf(s[i], "%d", &j)) printf("arg[%d] = '%s'\n", j);
+  //printf("arg[%d] = '%s'\n", i, s[i]);
+  
   return 0;
 }
