b36aa4aeb2528005d1903a7a5f5cfb68c9766597
kalachx <kalach.x@poczta.fm>
2016-03-14 09:44:30 +0100

    Update winbat.cmd

--- winbat.cmd
+++ winbat.cmd
@@ -1,8 +1,24 @@
 @ECHO OFF
 :Loop
 IF "%1"=="" GOTO Continue
+  if "%1"=="_" GOTO Got_
    echo %1
 SHIFT
 GOTO Loop
+
+:Got_
+  SHIFT
+  SHIFT
+  GOTO "%0"
+
+:Test1
+echo tst1
+goto Continue
+
+:Test2
+echo tst2
+goto Loop
+
 :Continue
 echo zakonczono!
+
