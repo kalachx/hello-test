d1e2a77fb90cb6c02a67234cc96ddfe8e29b2283
kalachx <kalach.x@poczta.fm>
2016-03-16 12:24:35 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,13 +2,11 @@
 
 int main(int n, char** s)
 {
-  printf("ilosc arg %d\n", n-1);
   printf("Zbudowano %s o godzinie %s\n", __DATE__, __TIME__);
+  printf("Ilosc arg %d\n", n-1);
   long i, j;
   for(i=1;i<n;i++) 
     if (sscanf(s[i], "%d", &j)) printf("arg[%d] = %d\n", i, j);
     else printf("arg[%d] = '%s'\n", i, s[i]);
-  //printf("arg[%d] = '%s'\n", i, s[i]);
-  printf("%d\n", (n==1));
-  return (n==1);
+  return 0
 }
